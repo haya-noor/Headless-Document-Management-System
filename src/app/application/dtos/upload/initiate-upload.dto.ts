78c6fe778e2f524883ec221bdd939e1a094d62a6
Haya_Noor <hayanoor.skt@gmail.com>
2025-10-24 16:51:40 +0500

    pulled code from day7

--- src/app/application/dtos/upload/initiate-upload.dto.ts
+++ src/app/application/dtos/upload/initiate-upload.dto.ts
@@ -1,15 +1,11 @@
 import { Schema as S } from "effect"
 import { DocumentId, UserId } from "@/app/domain/refined/uuid"
+import { DocumentVersionFields } from "@/app/domain/d-version/schema"
 
-// here .pick is not applicable because we are not picking 
-// fields from the one schema, we're picking fields form multiple schemas
-// filename, mimeType, size are picked from the metadata schema 
-export const InitiateUploadDTOSchema = S.Struct({
-  documentId: DocumentId,
-  userId: UserId,
-  filename: S.String,
-  mimeType: S.String,
-  size: S.Number
-})
+export const InitiateUploadDTOSchema = DocumentVersionFields
+  .pick("documentId", "filename", "mimeType", "size")
+  .pipe(S.extend(S.Struct({
+    userId: UserId
+  })))
 export type InitiateUploadDTO = S.Schema.Type<typeof InitiateUploadDTOSchema>
 export type InitiateUploadDTOEncoded = S.Schema.Encoded<typeof InitiateUploadDTOSchema>
